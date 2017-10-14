2547387f12ae98703fc2f392aaac3deda23bdba3
SimonIsMe <szym1991@gmail.com>
2017-10-14 20:45:05 +0200

    Debug

---
+++ install-dependencies.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+go get github.com/go-sql-driver/mysql
+go get github.com/gorilla/mux
+go get github.com/gorilla/rpc
+go get github.com/gorilla/rpc/json
+go get github.com/streadway/amqp
